6f5c1496c7bdc347f618b8f9d815f79fcadc50de
jaaabir <jaabir.zaid@gmail.com>
2025-10-08 16:28:37 +0100

    fddfggrfe

---
+++ test.py
@@ -0,0 +1,13 @@
+from encoder_decoder_model import load_pretrained_enc_dec_model
+import os 
+
+
+base_model_path = 'saved_models/mydit768_dbart/'
+ckpt = 'ocr/checkpoints/bdit_dbart/checkpoint-59500'
+
+print(f"base model path : {os.path.exists(base_model_path)}")
+print(f"ckpt path : {os.path.exists(ckpt)}")
+
+
+print(load_pretrained_enc_dec_model(ckpt, None, None, lora_applied=False))
+
